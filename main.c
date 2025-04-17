ebc6b20b3e7a7fe7a9850cb6d6b4369c2b6405f5
MoJail-SYSU <huangzhipeng122@outlook.com>
2025-04-17 23:44:15 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <regex.h>
-
-// 检查密码强度
-int check_password_strength(const char* password) {
-    int length = strlen(password);
-    int has_lower = 0, has_upper = 0, has_digit = 0, has_special = 0;
-
-    // 判断密码字符是否符合要求
-    for (int i = 0; i < length; i++) {
-        if (islower(password[i])) has_lower = 1;
-        else if (isupper(password[i])) has_upper = 1;
-        else if (isdigit(password[i])) has_digit = 1;
-        else if (ispunct(password[i])) has_special = 1;
-    }
-
-    // 根据密码的复杂度返回强度
-    if (length < 6) return 1; // 弱
-    if (length >= 6 && length < 8) {
-        if (has_lower && has_digit) return 2; // 中
-        return 1;
-    }
-    if (has_lower && has_upper && has_digit && has_special) return 3; // 强
-    return 2; // 中
-}
-
-// 输出密码强度
-void print_strength(int strength) {
-    switch (strength) {
-        case 1: printf("密码强度：弱\n"); break;
-        case 2: printf("密码强度：中\n"); break;
-        case 3: printf("密码强度：强\n"); break;
-        default: printf("无法评估密码强度\n"); break;
-    }
-}
 
 int main() {
     char password[100];
+    int length = 0;
+    int has_lower = 0;
+    int has_upper = 0;
+    int has_digit = 0;
+    int has_special = 0;
+    int i = 0;
 
     printf("请输入密码：");
-    scanf("%99s", password); // 获取用户输入的密码
+    scanf("%s", password);
 
-    int strength = check_password_strength(password);
-    print_strength(strength); // 输出密码强度
+    length = strlen(password);
+
+    // 逐个检查字符
+    while (password[i] != '\0') {
+        if (password[i] >= 'a' && password[i] <= 'z') {
+            has_lower = 1;
+        } else if (password[i] >= 'A' && password[i] <= 'Z') {
+            has_upper = 1;
+        } else if (password[i] >= '0' && password[i] <= '9') {
+            has_digit = 1;
+        } else {
+            has_special = 1;
+        }
+        i++;
+    }
+
+    // 开始根据长度和种类判断
+    if (length < 6) {
+        printf("密码强度：弱（太短啦！）\n");
+    } else {
+        int kind = has_lower + has_upper + has_digit + has_special;
+        if (length >= 8 && kind >= 3) {
+            printf("密码强度：强（棒棒哒！）\n");
+        } else if (kind >= 2) {
+            printf("密码强度：中（可以更好！）\n");
+        } else {
+            printf("密码强度：弱（需要改进哦～）\n");
+        }
+    }
+
+    // 小提示
+    if (!has_special) {
+        printf("提示：加入特殊符号可以提升安全性哦！\n");
+    }
+    if (length < 10) {
+        printf("提示：密码长度最好超过10位！\n");
+    }
 
     return 0;
 }
-
